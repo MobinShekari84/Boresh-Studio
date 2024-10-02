1b98dae83f22371a7e6125d1bc69fbd62789c539
Mobin Shekari <mobin.shekari84@gmail.com>
2024-10-02 20:03:43 +0330

    added dropdown menu

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function changeMenuIcon() {
     this.classList.toggle('xmenu');
+    const dropDownMenu = document.querySelectorAll('.dropDownMenu');
+    if (dropDownMenu[0].style.visibility == "visible") {
+        dropDownMenu[0].style.visibility = "hidden";
+        dropDownMenu[0].style.opacity = "0";
+    }
+    else {
+        dropDownMenu[0].style.visibility = "visible";
+        dropDownMenu[0].style.opacity = "1";
+    }
+}
+
+function changeDisplayDropDown() {
+    this.style.display = "block";
+    console.log(this);
 }
 
 const menuicon = document.querySelectorAll('.menuicon');
-
 menuicon.forEach(el => el.addEventListener('click', changeMenuIcon));
